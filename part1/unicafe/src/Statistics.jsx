022931ff3e9797b4fd1dec20e962e15d098c139e
Nazareno Valentini <nazareno.valentini@gmail.com>
2023-03-17 23:16:49 +0100

    1.11

--- part1/unicafe/src/Statistics.jsx
+++ part1/unicafe/src/Statistics.jsx
@@ -2,7 +2,6 @@ import StatisticLine from "./StatisticLine";
 
 export default function Statistics({ good, neutral, bad }) {
   const total = good + neutral + bad;
-  console.log(total);
   return (
     <>
       {total === 0 ? (
@@ -10,12 +9,14 @@ export default function Statistics({ good, neutral, bad }) {
       ) : (
         <>
           <h2>statistic</h2>
-          <StatisticLine text="good" value={good} />
-          <StatisticLine text="neutral" value={neutral} />
-          <StatisticLine text="bad" value={bad} />
-          <StatisticLine text="all" value={total} />
-          <StatisticLine text="average" value={(good - bad) / total} />
-          <StatisticLine text="positive" value={(good / total) * 100 + "%"} />
+          <table>
+            <StatisticLine text="good" value={good} />
+            <StatisticLine text="neutral" value={neutral} />
+            <StatisticLine text="bad" value={bad} />
+            <StatisticLine text="all" value={total} />
+            <StatisticLine text="average" value={(good - bad) / total} />
+            <StatisticLine text="positive" value={(good / total) * 100 + "%"} />
+          </table>
         </>
       )}
     </>
