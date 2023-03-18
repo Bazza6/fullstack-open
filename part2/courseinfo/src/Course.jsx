0f7298b7048ae330033bae500adac2162266f6f6
Nazareno Valentini <nazareno.valentini@gmail.com>
2023-03-18 13:08:30 +0100

    2.4 2.5

--- part2/courseinfo/src/Course.jsx
+++ part2/courseinfo/src/Course.jsx
@@ -1,11 +1,17 @@
 import Content from "./Content";
 import Header from "./Header";
 
-export default function Course({ course }) {
+export default function Course({ courses }) {
   return (
     <>
-      <Header title={course.name} />
-      <Content content={course.parts} />
+      {courses.map((course) => {
+        return (
+          <>
+            <Header title={course.name} />
+            <Content content={course.parts} />
+          </>
+        );
+      })}
     </>
   );
 }
